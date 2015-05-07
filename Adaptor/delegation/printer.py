52917e8dd51a334a64a164c2ba4216c9e1d83dce
nirperm <nirperm@gmail.com>
2015-05-07 06:49:57 +0000

    delegation(ongoing)

--- Adaptor/delegation/printer.py
+++ Adaptor/delegation/printer.py
@@ -1,17 +1,22 @@
-# from abc import ABCMeta
+# FIXME  not delegation i think
+# refer to http://qiita.com/ukisoft/items/b7c410b96dde1922a2d0
 from abc import *
+# from abc import ABCMeta
 
 
 class Printer():
     __metalcass__ = ABCMeta
 
     def __init__(self, string):
-        self.string = string
+        self.weak = self.print_weak(string)
+        self.strong = self.print_strong(string)
 
+    @classmethod
     @abstractmethod
-    def print_weak(self):
+    def print_weak(cls, string):
         pass
 
+    @classmethod
     @abstractmethod
-    def print_strong(self):
+    def print_strong(cls, string):
         pass
---
+++ Adaptor/delegation/printer_banner.py
@@ -0,0 +1,17 @@
+from printer import Printer
+# from banner import Banner
+
+
+class PrinterBanner(Printer):
+    # def __init__(self, string):
+    #    self.banner = Banner(string)
+
+    @classmethod
+    def print_weak(cls, string):
+        cls.weak
+        # self.banner.show_with_paren()
+
+    @classmethod
+    def print_strong(cls, string):
+        cls.strong
+        # self.banner.show_with_aster()
