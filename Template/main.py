f2a09fc964b60b07a8476b2fb56405843fb07419
nirperm <nirperm@gmail.com>
2015-05-08 06:40:35 +0000

    tweak japanese

--- Template/main.py
+++ Template/main.py
@@ -5,7 +5,7 @@ from string_display import StringDisplay
 if __name__ == '__main__':
     d1 = CharDisplay('H')
     d2 = StringDisplay('Hello, World')
-    d3 = StringDisplay('konnniciwa')
+    d3 = StringDisplay('こんにちわ')
 
     d1.display()
     d2.display()
