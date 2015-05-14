9db8b45fce7d28a6ab89a46708eadb368b3b8617
Nirperm <nirperm@gmail.com>
2015-05-14 18:42:02 +0900

    use super fucntion

--- Singleton/singleton.py
+++ Singleton/singleton.py
@@ -3,7 +3,7 @@ class Singleton(object):
 
     def __new__(cls, *args, **keys):
         if cls.__instance is None:
-            cls.__instance = object.__new__(cls)
+            cls.__instance = super(Singleton, cls).__new__(cls)
         return cls.__instance
 
     def __init__(self, name):
