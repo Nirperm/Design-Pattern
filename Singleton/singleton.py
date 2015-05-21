3ec7120d09e2e9b435d7ed72c97298fb679dc084
nirperm <nirperm@gmail.com>
2015-05-21 05:18:41 +0000

    tweak

--- Singleton/singleton.py
+++ Singleton/singleton.py
@@ -1,9 +1,9 @@
-class Singleton():
+class Singleton(object):
     __instance = None
 
     def __new__(cls, *args, **keys):
         if cls.__instance is None:
-            cls.__instance = super(Singleton, cls).__new__(cls)
+            cls.__instance = object.__new__(cls)
         return cls.__instance
 
     def __init__(self, name):
