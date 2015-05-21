4c03ffc356b69876a7c917197f4552e534e2c442
nirperm <nirperm@gmail.com>
2015-05-21 03:04:17 +0000

    Revert "finish Factory(refact)"
    This reverts commit 79c1d4bba889eec787f235e301368bc09f07285d.
    
    modified:   FactoryMethod/Framework/factory.py

--- FactoryMethod/Framework/factory.py
+++ FactoryMethod/Framework/factory.py
@@ -3,14 +3,14 @@ from abc import ABCMeta, abstractmethod
 
 class Factory(metaclass=ABCMeta):
     @abstractmethod
-    def _create_product(self, owner):
+    def _createproduct(self, owner):
         pass
 
     @abstractmethod
-    def _register_product(self, product):
+    def _registerproduct(self, product):
         pass
 
     def create(self, owner):
-        self.__p = self._create_product(owner)
-        self._register_product(self.__p)
+        self.__p = self._createproduct(owner)
+        self._registerproduct(self.__p)
         return self.__p
